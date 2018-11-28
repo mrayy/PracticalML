d9c9765d0c68020441d3c6d4aec18dd6a827f39a
MHD Yamen Saraiji <mrayyamen@gmail.com>
2018-11-28 13:25:50 +0900

    Fixing Session 2 compatibility with Python 3

--- 2. ML Algorithms/Python/OSCHelper.py
+++ 2. ML Algorithms/Python/OSCHelper.py
@@ -1,37 +1,58 @@
-from OSC import OSCServer
-from OSC import OSCClient, OSCMessage
+#from OSC import OSCServer
+#from OSC import OSCClient, OSCMessage
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+import threading
 import sys
 from time import sleep
 import types
 
-osc_run=True
+g_server=None
+
+class OSCServer:
+    def __init__(self,server,disp):
+        self._server=server
+        self._dispatcher=disp
+        self._running=False
+
+    def start(self):
+        self._running=True
+        while self._running:
+            self._server.handle_request()
+
+    def stop(self):
+        self._running=False
+        #self._server.server_close()
+        #self._server.shutdown()
+
+    def close(self):
+        self._running=False
+        self._server.server_close()
+
+    def addMsgHandler(self,path,func):
+        self._dispatcher.map(path,func)
+
 def createServer(port):
-    server = OSCServer( ("localhost",port) )
-    server.timeout = 0
-    def handle_timeout(self):
-        self.timed_out = True
-    server.handle_timeout = types.MethodType(handle_timeout, server)
-    server.addMsgHandler( "/quit", onOSC_quit )
+    disp = dispatcher.Dispatcher()
+    oscServ = osc_server.BlockingOSCUDPServer(("localhost",port), disp)
+    disp.map( "/quit", onOSC_quit )
+    server=OSCServer(oscServ,disp)
     return server
 
 def createClient(port):
-    client = OSCClient()
-    client.connect( ("localhost", port) )
+    client = udp_client.SimpleUDPClient( "localhost", port )
     return client
 
-def each_frame(server):
-    server.timed_out = False
-    while not server.timed_out:
-        server.handle_request()
 
-
-def onOSC_quit(path, tags, args, source):
-    global osc_run
-    osc_run = False
+def onOSC_quit(*args):
+    global g_server
+    g_server.stop()
 
 def start_server(server):
-    global osc_run
-    osc_run=True
-    while osc_run:
-        sleep(1)
-        each_frame(server)
+    global g_server
+    g_server=server
+    g_server.start()
